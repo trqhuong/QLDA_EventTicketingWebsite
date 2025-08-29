6fac5b3039eb3f8604a486e1c5acf8b93420b696
trqhuong <quynhhuongtran314@gmail.com>
2025-08-29 10:11:37 +0700

    BVSKTT-15 giao dien thong ke

--- app/index.py
+++ app/index.py
@@ -8,5 +8,29 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/organizer")
+def organizer_header():
+    return render_template("organizer.html")
+
+@app.route("/create_event")
+def create_event():
+    return render_template("create_event.html")
+
+@app.route("/my_event")
+def my_event():
+    return render_template("my_event.html")
+
+@app.route("/report_event")
+def report_event():
+    return render_template("report_event.html")
+
+@app.route("/my_ticket")
+def my_ticket():
+    return render_template("my_ticket.html")
+
+@app.route("/book_ticket")
+def book_ticket():
+    return render_template("book_ticket.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
